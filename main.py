b1bf62f16b01fb1c84f5e2bfb7149a170923e5d8
unknown <stergiadis777@gmail.com>
2022-02-20 23:50:15 +0100

    fixed extensions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# bot.py
 import os
 import discord
 import datetime
@@ -6,12 +7,10 @@ import os
 with open("secret.txt", "r", encoding='utf-8') as f:
     TOKEN = f.readlines()[0].strip()
 
-# Lol name malaka fortuna.
-GUILD = 'group'
-STATE_FILE = 'state.tsv.txt'
-
-# Assuming Eulalia knows what he is talking about.
+GUILD = 'test'
+STATE_FILE = 'state.tsv'
 WINDOW = (datetime.timedelta(hours=12), datetime.timedelta(hours=21))
+
 
 class BossNotFound(Exception):
     pass
@@ -19,26 +18,28 @@ class BossNotFound(Exception):
 class ToDInFuture(Exception):
     pass
 
-# Make stuff happen
 client = discord.Client()
 
-###############
-### Helpers ###
-###############
+@client.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(guild)
+    global state
+    state = get_state()
+
 def check(tod):
     start = tod + WINDOW[0]
     end = tod + WINDOW[1]
     now = datetime.datetime.now()
-    print("Start: ", start, "now", now, "end", end)
-    print("Therefore", start <= now <= end)
     if start <= now <= end:
-        return "On Window"
+        return "**On Window**"
     if now < start:
         delta = start - now
         h, m = delta.seconds // 3600, (delta.seconds // 60) % 60
         return "Window starts in {} hours and {} minutes".format(h, m)
     if now > end:
         return "Window ended {} ago, we probably didn't register the last ToD".format(now - end)
+    
 
 def get_supported_bosses():
     return list(state.keys())
@@ -53,6 +54,9 @@ def get_state():
         state = {line.split("\t")[0]: str2date(line.split("\t")[1]) for line in lines}
     return state
 
+def troll(name):
+    return "**The 9000 IQ supreme leader {} has spoken, all shall listen to his infinite wisdom.**".format(name)
+
 def set_state(state):
     os.remove(STATE_FILE)
     with open(STATE_FILE, "w", encoding='utf-8') as f:
@@ -62,7 +66,7 @@ def set_state(state):
 
 
 def register(name, time, date=None):
-    if name not in state:
+    if name not in state and not force:
         raise BossNotFound()
         
     # Update the state
@@ -88,26 +92,14 @@ def register(name, time, date=None):
     
     state[name] = tod
     set_state(state)
-    
-######################
-### Event Handlers ###
-######################
-@client.event
-async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-    global state
-    state = get_state()
-    
 @client.event
 async def on_message(message):
     if message.author == client.user or not message.content.startswith("!"):
         return
-    
+    if str(message.channel) != "bots":
+        return
+
     if message.content == '!check':
         response = "I was called"
         for name in get_supported_bosses():
@@ -131,11 +123,11 @@ async def on_message(message):
 
        except BossNotFound:
            await message.channel.send("Boss {} unknown, use !sb to check which bosses are currently supported".format(name))
+           await message.channel.send("If you wish to register a new boss, repeat the same command but using !fregister instead of !register")
        except ToDInFuture:
            msg = "ToD cannot lie in the future silly. Are you sure you are using CET timezone?"
            await message.channel.send(msg)
        except ValueError as e:
            await message.channel.send(str(e))
 
-# Launch
 client.run(TOKEN)
